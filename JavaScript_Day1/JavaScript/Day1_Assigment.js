5e8c7885601eeb95b8d7905686d87ee3a3a528cb
Sakshinde2512 <85336764+Sakshinde2512@users.noreply.github.com>
2023-05-09 18:38:33 +0530

    Add files via upload

---
+++ JavaScript_Day1/JavaScript/Day1_Assigment.js
@@ -0,0 +1,17 @@
+document.write("</br>");
+//printing name
+let myname = "Sakshi Shinde"
+document.write("My Name is "+"<i>" +myname+"</i>")
+
+document.write("</br>");
+
+    //printing city
+   let city = "Mumbai"
+	document.write("My Birth City is "+ city);
+
+  document.write("</br></br>Printing through JS")
+  var hobbies=["Reading", "Listening Music","Swimming"]
+  document.write("Here are my hobbies: </br>")
+  document.write("<ul><li>"+ hobbies[1]+ "</li>")
+  document.write("<li>"+ hobbies[1]+ "</li><li>"+ hobbies[2] +"</li></ul>");
+
